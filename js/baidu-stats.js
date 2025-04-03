ea47931e8aeda2d9352dcb95340d5f8420c0d856
ug1y <yinhao@icode.pku.edu.cn>
2025-04-03 17:26:27 +0800

    动态加载多个页面，梳理清晰项目结构

---
+++ js/baidu-stats.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?acaec23522dc5cf1a7e371d257f6d65f";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
